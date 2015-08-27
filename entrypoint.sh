b51f092ceda27c60ac46700e8b2f40ef0c78677a
amsatique <amsatique@users.noreply.github.com>
2015-08-27 13:01:52 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-set -e
+set -m
 
-if [ ! -z "${USERNAME}" ] && [ ! -z "${PWDSUPERVISOR}" ]
-then
-    sed -i 's#username = Username#username = '"$USERNAME"'#g' /etc/supervisord.conf
-    sed -i 's#password = PwdSupervisor#password = '"$PWDSUPERVISOR"'#g' /etc/supervisord.conf
+wget -O mongo.tgz https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel70-${VERSION}.tgz 
+tar -xvf mongo.tgz -C /usr/local --strip-components=1 
+rm -f mongo.tgz
+
+mongodb_cmd="mongod --storageEngine wiredTiger"
+if [ "${RS_NAME}" != "**None**" ]; then
+	cmd="$mongodb_cmd --dbpath /var/lib/mongo --directoryperdb --replSet ${RS_NAME}"
+else
+	cmd="$mongodb_cmd --dbpath /var/lib/mongo --directoryperdb"
 fi
-
-exec "$@"
+$cmd &
+fg
